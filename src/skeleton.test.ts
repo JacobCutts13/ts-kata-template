9d9b17c397db66e9c29289f80addbbce922ebbcd
Jc <Jc@email.com>
2022-02-28 09:45:41 +0000

    added tests to jest

--- src/skeleton.test.ts
+++ src/skeleton.test.ts
@@ -1,5 +1,15 @@
-import sum from "./skeleton";
+import minToHour from "./skeleton";
 
-test("sum adds two numbers", () => {
-  expect(sum(4, 3)).toBe(7);
+test("minToHours converts total minutes to hours and minutes", () => {
+  expect(minToHour(3600)).toBe("1 hour (s) and 0 minute(s)");
+  expect(minToHour(3601)).toBe("1 hour (s) and 1 minute(s)");
+  expect(minToHour(323500)).toBe("89 hour (s) and 51 minute(s)");
+  expect(minToHour(30)).toBe("0 hour (s) and 30 minute(s)");
 });
+
+/* Tests
+3600 --> "1 hour(s) and 0 minute(s)"
+3601 --> "1 hour(s) and 0 minute(s)"
+3500 --> "0 hour(s) and 58 minute(s)"
+323500 --> "89 hour(s) and 51 minute(s)"
+*/
--- src/skeleton.ts
+++ src/skeleton.ts
@@ -8,3 +8,5 @@ function minToHour(minutes: number): string {
 }
 
 export default minToHour;
+
+
